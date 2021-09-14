8ed59b91696146ee9bdccd0ba61413ec6e793df3
Bretlirn <vinsonchen222@gmail.com>
2021-09-13 23:02:24 -0700

    Finished assignments

---
+++ com.ss.fs.firstasgn/src/Assignment1.java
@@ -0,0 +1,66 @@
+public class Assignment1 {
+    public static void main(String[] args) throws Exception {
+        for (int i = 1; i <= 4; i++){
+            System.out.println(i+")");
+
+            if (i % 2 == 1){
+                printStars(i);
+                printLines(i);
+            }
+            else{
+                printLines(i);
+                printStars(i);
+            }
+        }
+    }
+
+
+    public static void printStars(Integer n){
+        if (n == 1){
+            for (int i = 1; i <= 4; i++){
+                for(int j = 0; j < i; j++){
+                    System.out.printf("*");
+                }
+                System.out.printf("\n");
+            }
+        }
+        if (n == 2){
+            for (int i = 4; i > 0; i--){
+                for(int j = 0; j < i; j++){
+                    System.out.printf("*");
+                }
+                System.out.printf("\n");
+            }
+        }
+        if (n == 3){
+            for (int i = 0; i < 4; i++){
+                for (int j = 0; j < 5 - i; j++){
+                    System.out.printf(" ");
+                }
+                for(int j = 0; j < (2 * i) + 1; j++){
+                    System.out.printf("*");
+                }
+                System.out.printf("\n");
+            }
+        }
+        if (n == 4){
+            for (int i = 0; i < 4; i++){
+                for (int j = 0; j < 2 + i; j++){
+                    System.out.printf(" ");
+                }
+                for(int j = 0; j < 7 - (2 * i); j++){
+                    System.out.printf("*");
+                }
+                System.out.printf("\n");
+            }
+        }
+    }
+
+    public static void printLines(Integer n){
+        System.out.printf("........");
+        for (int i = 0; i < n; i++){
+            System.out.printf(".");
+        }
+        System.out.printf("\n");
+    }
+}
---
+++ com.ss.fs.firstasgn/src/Assignment2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Assignment2 {
+    public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
+        Random rand = new Random();
+        int r = rand.nextInt(100) + 1;
+        int nextInt;
+        System.out.printf("Guess a number between 1 - 100: ");
+        for (int i = 0; i < 5; i++){
+            nextInt = s.nextInt();
+            if (Math.abs(r-nextInt) < 10){
+                System.out.println("You are within 10 digits away! The number is: "+r);
+                return;
+            }
+            if (i != 4){
+                System.out.printf("You are not correct. Guess again (1-100): ");
+            }
+            else{
+                System.out.println("You are out of tries!\nTry again next time!");
+            }
+        }
+        s.close();
+    }
+}
