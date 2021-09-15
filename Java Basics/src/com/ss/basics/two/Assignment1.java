61d0b3c082d349e2a02b186eac1dcabd2752f936
Bretlirn <vinsonchen222@gmail.com>
2021-09-15 09:52:59 -0700

    reorganized file again

---
+++ Java Basics/src/com/ss/basics/two/Assignment1.java
@@ -0,0 +1,17 @@
+package com.ss.basics.two;
+
+public class Assignment1 {
+    public static void main(String[] args){
+        int total = 0;
+        for (String arg: args){
+            try{
+                Integer i = Integer.parseInt(arg);
+                total = total + i;
+            } 
+            catch (Exception e){
+                System.out.printf("'%s' cannot be turned into an integer!\n", arg);
+            }
+        }
+        System.out.printf("Sum of the integer arguments: %d\n", total);
+    }
+}
